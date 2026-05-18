c54030f997b7d90b36ed522adaaaa2f99b24226c
Gonzalo Sebastian Koc Alavedra <gska2010@gmail.com>
2026-05-17 23:05:35 -0500

    Cambios en botones de Home

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -88,7 +88,7 @@ const handleLogout = async () => {
         <li className="nav-dot">·</li>
       </ul>
 
-                <Link href="/earlyaccess" className="nav-cta-btn">
+                <Link href="/home#cta" className="nav-cta-btn">
           <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
             <path
               d="M3.61 1.814L13.793 12 3.61 22.186a.996.996 0 0 1-.61-.92V2.734a1 1 0 0 1 .61-.92zm.96-.794l11.18 6.456L12.8 10.43 4.57 1.02zM16.75 8.476l2.87 1.655c.75.433.75 1.305 0 1.738l-2.87 1.655-3.28-3.524 3.28-3.524zM4.57 22.98l8.23-9.41 2.95 2.954L4.57 22.98z" />
